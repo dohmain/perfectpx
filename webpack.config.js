46db195f5235c41a6cdaf5aec2d0debee62b3f62
dohmain <jdoh29@gmail.com>
2020-02-11 09:04:44 -0500

    small adjustments

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@
 var path = require('path');
 
 module.exports = {
-  entry: './frontend/index.jsx',
+  context: __dirname,
+  entry: './frontend/perfectpx.jsx',
   output: {
-    filename: './bundle.js',
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
