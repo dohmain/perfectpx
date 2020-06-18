16ad3e041fb1eed9ab45591aa0c82f4ed2e574f7
dohmain <jdoh29@gmail.com>
2020-06-18 16:28:38 -0400

    refactor TopNav

--- frontend/components/top_nav/top_nav.jsx
+++ frontend/components/top_nav/top_nav.jsx
@@ -4,11 +4,70 @@ import { Link } from 'react-router-dom';
 class TopNav extends React.Component {
   constructor(props){
     super(props);
+    debugger;
+    this.currentUser = this.props.currentUser;
+    this.onclick = this.onclick.bind(this);
+    this.handleDropdown = this.handleDropdown.bind(this);
+
+    // this.forceUpdate = this.forceUpdate.bind(this);
+    // require this component to re-render whenever the store's state changes
+    // this.props.store.subscribe(this.forceUpdate);
   }
 
+  onclick(event) {
+    if (!event.target.matches('.header-user-dropbtn')) {
+      let dropdowns = document.getElementsByClassName('header-user-dropdown-content');
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  handleDropdown() {
+    return document.getElementById('userDropdown').classList.toggle('show');
+  };
+
   render() {
+    const right_nav = this.currentUser ? (
+      <>
+        <div className='header-user-dropdown'>
+          <img src={window.userHeaderIconURL} onClick={() => this.handleDropdown()} className='header-user-dropbtn header-user-icon' />
+          <div id='userDropdown' className='header-user-dropdown-content'>
+            <Link to={`/users/${this.currentUser.id}`} className='dropdown-link'>Profile</Link>
+            <Link to={`/users/${this.currentUser.id}/my_information`} className='dropdown-link'>Settings</Link>
+            <br/>
+            <a className='dropdown-link' onClick={this.props.logout}>Log out</a>
+          </div>
+        </div>
+        <Link to='/upload' className='header-upload-button'>Upload</Link>
+      </>
+    ) : (
+      <>
+        <Link to='/login' className='header-login-button'>Log In</Link>
+        <Link to='/signup' className='header-signup-button'>Sign Up</Link> 
+      </>
+    )
+  
     return (
-      <div> header </div>
+      <div className='top-nav-container'>
+        <div className='top-nav-logo-link-container'>
+          <div className='logo-link-container'>
+            <Link to='/' className='logo-link'>PERFECTpx</Link>
+            </div>
+          <div className='top-nav-link-container'>
+            <Link to='/discover' className='top-nav-link'>Discover</Link>
+            <a href='#' className='top-nav-link'>LinkedIn</a>
+            <a href='#' className='top-nav-link'>Github</a>
+          </div>
+        </div>
+        <div className='top-nav-right'>
+          {right_nav}
+        </div>
+      </div>
     )
   }
 }
--- frontend/components/top_nav/top_nav_container.js
+++ frontend/components/top_nav/top_nav_container.js
@@ -12,4 +12,4 @@ const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout())
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TopNav));
+export default connect(mapStateToProps, mapDispatchToProps)(TopNav);
