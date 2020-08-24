c5e1bff667b12be0e3a0b33a0de845d473510202
dohmain <jdoh29@gmail.com>
2020-08-24 19:54:16 -0400

    cleanup

--- frontend/components/comments/comments.jsx
+++ frontend/components/comments/comments.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 
 class Comments extends React.Component {
-  // constructor(props){
-  //   super(props)
-  // }
-
-  // componentDidMount() {
-    
-  // }
 
   render() {
-    // let comments = this.props.comments.map(comment => {
-    //   return (
-    //     <li className='comment-list-item' key={`comment-${comment.id}`}>{comment.body}</li> 
-    //   )
-    // })
 
     let comments = this.props.comments.map(comment => {
       return (
