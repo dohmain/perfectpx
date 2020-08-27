ab466884b4550675d5950cfbd625f51624296e66
dohmain <jdoh29@gmail.com>
2020-08-27 13:17:26 -0400

    small adjustment

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,6 +1,6 @@
 class Api::PhotosController < ApplicationController
   def index
-    @photos = Photo.with_attached_px.all.includes(:creator)
+    @photos = Photo.with_attached_px.includes(:creator)
     render :index
   end
 
