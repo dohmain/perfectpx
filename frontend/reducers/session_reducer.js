0383c8b336b3b9cc099bfc7ef729f6387dc59b4d
dohmain <jdoh29@gmail.com>
2020-09-18 23:14:44 -0400

    comment things to do

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,4 +1,6 @@
+import { RECEIVE_PHOTO } from '../actions/photo_actions';
 import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_FOLLOW, UNFOLLOW_USER } from '../actions/follow_actions';
 
 export default (state = {}, action) => {
   Object.freeze(state);
@@ -10,10 +12,17 @@ export default (state = {}, action) => {
     case LOGOUT_CURRENT_USER:
       return {};
 
+    case RECEIVE_FOLLOW:
+      return state;
+      // update with reference to new follow
+
     case UNFOLLOW_USER: 
       return state;
       // delete the follow referece from old state and return new state
 
+    case RECEIVE_PHOTO: 
+      return state;
+      // update with reference to new photo
       
     default:
       return state;
