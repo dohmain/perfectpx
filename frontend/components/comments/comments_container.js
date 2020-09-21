b77608d3232870fa3158e2bfb5351fbb0b935ffe
dohmain <jdoh29@gmail.com>
2020-09-21 12:53:13 -0400

    cleanup

--- frontend/components/comments/comments_container.js
+++ frontend/components/comments/comments_container.js
@@ -1,22 +1,20 @@
 import { connect } from 'react-redux';
 import { postComment, clearErrors, getComments } from '../../actions/comment_actions'
 import Comments from './comments';
-import { withRouter } from 'react-router';
 
-const mapStateToProps = (state, ownProps) => {
-  debugger;
-  return ({
+const mapStateToProps = (state) => {
+  return {
     currentUserId: state.session.id ? state.session.id : null,
     errors: state.entities.errors
-  })
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-  return ({
+  return {
     postComment: (comment) => dispatch(postComment(comment)),
     clearErrors: () => dispatch(clearErrors()),
     getComments: () => dispatch(getComments()),
-  })
+  }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Comments))
+export default connect(mapStateToProps, mapDispatchToProps)(Comments)
