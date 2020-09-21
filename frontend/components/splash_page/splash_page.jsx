4126cec69961762cacdd8d6a69e45ac901f64d96
dohmain <jdoh29@gmail.com>
2020-09-21 18:44:42 -0400

    fontawesome

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -28,14 +28,17 @@ class SplashPage extends React.Component {
             </div>
             <div className="different-items">
               <div className="different-item">
+                <i className="fas fa-seedling fa-4x"></i>
                 <h3>Grow as a photographer</h3>
                 <p>Get immediate exposure with your first upload. Our Pulse algorithm surfaces new photographs and photographers, ensuring your photos are seen by the community so you receive valuable feedback on day one.</p>
               </div>
               <div className="different-item">
+                <i className="fas fa-pencil-ruler fa-4x"></i>
                 <h3>Build your career</h3>
                 <p>Present yourself as a professional. Get hired by displaying your services, create a Directory listing, showcase the workshops you're holding, and create Galleries to highlight your work.</p>
               </div>
               <div className="different-item">
+                <i className="far fa-chart-bar fa-4x"></i>
                 <h3>See how you're performing</h3>
                 <p>With Statistics and Pulse you get valuable insights into how your photos are performing and how you rank in comparison to other photographers in the community.</p>
               </div>
