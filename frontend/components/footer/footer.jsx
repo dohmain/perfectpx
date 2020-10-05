6d2de7d260e378d4865e4b49b08219df14477b6a
dohmain <jdoh29@gmail.com>
2020-10-05 16:56:19 -0400

    attributions and styling

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,7 +4,15 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <hr />
-      <h3>&copy; PERFECTpx</h3>
+      <div id='footer-sub-container'>
+        <div id='footer-left'>
+          <span>&copy; PERFECTpx</span>
+        </div>
+        <div id='footer-right'>
+          <span className="footer-attrib" id='footer-icons'>icons: <a href='https://fontawesome.com/' target='_blank'>fontawesome</a></span>
+          <span className="footer-attrib" id='footer-photos'>photos: <a href='https://unsplash.com/' target='_blank'>unsplash</a></span>
+        </div>
+      </div>
     </div>
   )
 }
